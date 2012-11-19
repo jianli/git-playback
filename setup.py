67c50e9d79d3ce768d5c672afd9987bf85efddc4
Jian Li <jian@jianli.us>
2012-11-18 18:38:20 -0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='git-playback',
+    version='0.1',
+    url='http://github.com/jianlius/git-playback',
+    packages=find_packages('.'),
+    entry_points={
+        'console_scripts': (
+            'git-playback = playback:playback',
+        ),
+    },
+    install_requires=[
+        'gitpython',
+    ],
+)
